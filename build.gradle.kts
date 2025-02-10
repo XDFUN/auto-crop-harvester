3802fe67678b0a2070dda034cbeed0d2b703282a
XDFUN <blechhosenheinz@gmail.com>
2025-02-10 19:46:28 +0100

    ci: added publish to ci using publishMod from modmuss50

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,20 @@ plugins {
     `maven-publish`
     kotlin("jvm") version "2.1.0"
     kotlin("plugin.serialization") version "2.1.10"
+    id("me.modmuss50.mod-publish-plugin") version "0.4.5"
 }
 
-version = project.properties["mod_version"] ?: throw IllegalStateException("mod_version must be set")
+version = providers.environmentVariable("mod_version").orNull ?: project.properties["mod_version"]
+        ?: throw IllegalStateException("mod_version must be set")
+project.version = version
 group = project.properties["maven_group"] ?: throw IllegalStateException("maven_group must be set")
 
+val minecraftVersion: String =
+    project.properties["minecraft_version"]?.toString() ?: throw IllegalStateException("minecraft_version must be set")
+
 base {
-    archivesName = project.properties["archives_base_name"]?.toString() ?: throw IllegalStateException("archives_base_name must be set")
+    archivesName = project.properties["archives_base_name"]?.toString()
+        ?: throw IllegalStateException("archives_base_name must be set")
 }
 
 repositories {
@@ -37,7 +44,7 @@ loom {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     // To change the versions see the gradle.properties file
-    minecraft("com.mojang:minecraft:${project.properties["minecraft_version"]}")
+    minecraft("com.mojang:minecraft:${minecraftVersion}")
     mappings("net.fabricmc:yarn:${project.properties["yarn_mappings"]}:v2")
     modImplementation("net.fabricmc:fabric-loader:${project.properties["loader_version"]}")
 
@@ -86,5 +93,58 @@ publishing {
             artifact(base.archivesName)
             from(components["java"])
         }
+    }
+
+    val mavenUrl = project.providers.environmentVariable("MAVEN_URL").orNull
+
+    if (mavenUrl != null) {
+        repositories.maven {
+            url = uri(mavenUrl)
+
+            val mavenUsername = project.providers.environmentVariable("MAVEN_USERNAME").orNull
+
+            if (mavenUsername != null) {
+                credentials(PasswordCredentials::class) {
+                    username = mavenUsername
+                    password = project.providers.environmentVariable("MAVEN_PASSWORD").toString()
+                }
+            }
+        }
+    }
+}
+
+publishMods {
+    file = tasks.remapJar.get().archiveFile.get().asFile
+    changelog = providers.environmentVariable("CHANGELOG").getOrElse("No changelog provided")
+    type.set(if (project.properties["prerelease"] == "true") BETA else STABLE)
+    modLoaders.add("fabric")
+
+    val curseforgeAccessToken: String? = null
+    val curseforgeMinecraftVersion = providers.environmentVariable("curseforge_minecraft_version").get()
+
+    val modrinthToken = project.providers.environmentVariable("MODRINTH_TOKEN").orNull
+
+    val githubToken: String? = providers.environmentVariable("GITHUB_TOKEN").orNull
+
+    // Currently not setup
+//    curseforge {
+//        dryRun = curseforgeAccessToken == null
+//        accessToken = providers.environmentVariable("CURSEFORGE_API_KEY")
+//        projectId = "306612"
+//        minecraftVersions.add(curseforgeMinecraftVersion)
+//    }
+
+    modrinth {
+        dryRun = modrinthToken == null
+        accessToken = modrinthToken
+        projectId = "h6cMODvx"
+        minecraftVersions.add(minecraftVersion)
+    }
+
+    github {
+        dryRun = githubToken == null
+        accessToken = githubToken
+        repository = providers.environmentVariable("GITHUB_REPOSITORY").getOrElse("AutoCropHarvester/dryrun")
+        commitish = providers.environmentVariable("GITHUB_REF_NAME").getOrElse("dryrun")
     }
 }
